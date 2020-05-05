76b4d996ba2e20d1773b7258754665f0cdaeecc6
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2020-05-05 14:53:57 +0800

    [add] Add createFromOverHours

---
+++ pages/api/checkinLog/createFromOverHours.js
@@ -0,0 +1,59 @@
+import { PrismaClient } from '@prisma/client'
+
+import { authMiddleware, runMiddleware } from '../../../util/apiMiddlewares'
+
+const prisma = new PrismaClient()
+
+// POST /api/checkinLog/create
+export default async function handle(req, res) {
+    await runMiddleware(req, res, authMiddleware);
+
+    const { studentId, name } = req.body;
+
+    if (!studentId) {
+        res.status(400).json({ ok: false, error: "missing_field" });
+        return;
+    }
+
+    const user = await prisma.user.findOne({
+        where: {
+            studentNumber: studentId
+        }
+    })
+
+    if (!user) {
+        await prisma.user.create({
+            data: {
+                studentNumber: studentId,
+                name
+            }
+        });
+    }
+
+    const checkinLog = (await prisma.checkinLog.findMany({
+        where: {
+            user: {
+                studentNumber: studentId
+            }
+        }
+    })).filter((v) => {
+        return v.checkinAt.toDateString() === new Date().toDateString()
+    })
+
+    if (checkinLog.length > 0) {
+        res.status(422).json({ ok: false, error: "already_checkin" });
+        return
+    }
+
+    const result = await prisma.checkinLog.create({
+        data: {
+            user: {
+                connect: { studentNumber: user.studentNumber }
+            },
+            checkinAt: new Date(),
+            temperature
+        }
+    })
+
+    res.json({ ok: true, result })
+}
